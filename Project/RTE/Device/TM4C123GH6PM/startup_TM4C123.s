8204dba704f9ccb5057348878bfd8f88ddd97a53
Abishek <abishek0398@gmail.com>
2019-03-07 19:29:43 +0530

    updated with the latest changes

--- Project/RTE/Device/TM4C123GH6PM/startup_TM4C123.s
+++ Project/RTE/Device/TM4C123GH6PM/startup_TM4C123.s
@@ -231,10 +231,10 @@ __Vectors_Size  EQU     __Vectors_End - __Vectors
 Reset_Handler   PROC
                 EXPORT  Reset_Handler             [WEAK]
                 IMPORT  SystemInit
-                IMPORT  __main
+                IMPORT  main
                 LDR     R0, =SystemInit
                 BLX     R0
-                LDR     R0, =__main
+                LDR     R0, =main
                 BX      R0
                 ENDP
 
