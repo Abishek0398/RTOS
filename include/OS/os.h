fd9f4773426c84ed1664d9a154c0f8151968d3a1
Abishek <abishek0398@gmail.com>
2019-10-03 23:09:56 +0530

    Added additional declarations for semaphore implementation and providing argument support for tasks

--- include/OS/os.h
+++ include/OS/os.h
@@ -1,20 +1,50 @@
 #ifndef OS_H
 #define OS_H
-#define NULL (void*)(0)
-#define READY 0
-#define WAITING 1
-#define RUNNING 2
-#define BLOCKED 3
-#include <stdint.h>
-extern uint8_t start_flag;
-extern void context_switch(void);
-extern void scheduler();
-extern void os_init();
-extern void os_first_task();
+#include <OS/tasks.h>
+struct sem_data
+{
+	struct tcb *tcb_ptr;
+	struct sem_data *next;
+	struct sem_data *prev;
+};
+
+struct semaphore
+{
+	struct sem_data *sem_ptr_head;
+	struct sem_data *sem_ptr_tail;
+	int16_t sem_count;
+};
+
+struct mutex
+{
+	struct tcb *owner_tcb;
+	struct sem_data *mut_ptr_head;
+	struct sem_data *mut_ptr_tail;
+	uint8_t highest_prio;
+	uint8_t lock;
+};
+extern void os_sem_create(struct semaphore *sem, uint16_t);
+extern void os_sem_wait(struct semaphore *sem);
+extern void os_sem_signal(struct semaphore *sem);
+extern void os_block(struct semaphore *sem);
+extern void os_release(struct semaphore *sem);
+extern void os_sem_delete(struct semaphore *sem);
+//extern void os_mutex_create();
+extern struct sem_data *os_delete_semqueue(struct semaphore *sem);
+extern void os_add_semqueue(struct semaphore *sem, struct sem_data *temp);
+extern uint8_t os_sched_state;
+extern void os_context_switch(void);
+extern void os_scheduler(void);
+extern void os_init(void);
+extern void os_first_task(void);
+extern void os_change_prio(struct tcb *, uint8_t);
 extern struct tcb *new_high_tcb;
 extern struct tcb *current_tcb;
-//extern uint32_t counter;
-extern void start_critical();
-extern void end_critical();
-void os_start(void);
+extern uint8_t os_leading_zeros(uint32_t);
+extern uint8_t os_get_highest_priority(void);
+extern void os_add_ready_list(struct tcb *temp);
+extern void os_remove_ready_list(struct tcb *temp);
+extern void os_start_critical(void);
+extern void os_end_critical(void);
+extern void os_start(void);
 #endif
